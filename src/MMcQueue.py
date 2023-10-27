10ff65e0670de24ac413ea6da43140dedd90f0cc
Alessio Celentano <alessiocelentano2003@gmail.com>
2023-10-27 17:23:56 +0200

    Add simulation

---
+++ src/MMcQueue.py
@@ -0,0 +1,96 @@
+import math
+import random
+import simpy
+
+
+class MMcQueue:
+    def __init__(self, num_servers, arrival_rate, service_rate, num_customers):
+        self.num_servers = num_servers
+        self.arrival_rate = arrival_rate
+        self.service_rate = service_rate
+        self.num_customers = num_customers
+
+        self.server_utilization = arrival_rate / (num_servers * service_rate)
+        self.state_0_probability = self._get_state_0_probability()
+        self.queue_probability = self._c_erlang(num_servers, self.server_utilization)
+        self.average_queue_length = self.queue_probability * (self.server_utilization / (1 - self.server_utilization))
+        self.average_service_length =  num_servers * self.server_utilization
+        self.average_system_length = self.average_queue_length + self.average_service_length
+        self.average_queue_waiting_time = self.average_queue_length / (num_servers * service_rate * (1 - self.server_utilization))
+        self.average_system_waiting_time = self.average_system_length / (num_servers * service_rate * (1 - self.server_utilization))
+        
+        self.customers_history = {}
+        self.service_lengths = []
+        self.system_lengths = []
+        self.queue_lengths = []
+        self.system_waiting_times = []
+        self.queue_waiting_times = []
+        
+    def run(self):
+        env = simpy.Environment()
+        server = simpy.Resource(env, capacity=self.num_servers)
+        env.process(self._generate_arrivals(env, server))
+        env.run()
+
+    def get_state_probability(self, k):
+        if k < self.servers:
+            return self.state_0_probability * (((self.num_servers * self.server_utilization)**k) / math.factorial(k))
+        return self.state_0_probability * (((self.num_servers * self.servers_utilization)**k) \
+            * (self.num_servers**(self.num_servers - k))) / math.factorial(self.num_servers)
+
+    def _generate_arrivals(self, env, server):
+        for i in range(self.num_customers):
+            interarrival_time = random.expovariate(self.arrival_rate)
+            yield env.timeout(interarrival_time)
+            env.process(self._generate_services(env, server,f"Cliente {i+1}"))
+            self.service_lengths.append(server.count)
+            self.queue_lengths.append(len(server.queue))
+            self.system_lengths.append(server.count + len(server.queue))
+
+    def _generate_services(self, env, server, name):
+        arrival_time = env.now
+        print(f"{name} arriva al tempo {arrival_time}")
+        self.customers_history[arrival_time] = server.count + len(server.queue)
+
+        with server.request() as req:
+            yield req
+
+            service_start = env.now
+            print(f"{name} inizia il servizio al tempo {service_start}")
+
+            service_duration = random.expovariate(server.count * self.service_rate)
+            yield env.timeout(service_duration)
+
+            service_end = env.now
+            print(f"{name} completa il servizio al tempo {service_end}")
+            self.customers_history[service_end] = server.count + len(server.queue)
+            
+            time_in_queue = service_end - service_start
+            time_in_system = time_in_queue + (service_start - arrival_time)
+            self.queue_waiting_times.append(time_in_queue)
+            self.system_waiting_times.append(time_in_system)
+            
+    def _c_erlang(self, c, a):
+        den = 0
+        for i in range(c):
+            den += (((c * a)**i) / math.factorial(i))
+        den *= (1 - a) * (math.factorial(c) / ((c * a)**c))
+        den += 1
+        return 1 / den
+        """
+        num = ((a**c) / math.factorial(c)) * (1 / (1 - a))
+        den = ((a**c) / math.factorial(c)) * (1 / (1 - a))
+        for i in range(c):
+            den += ((a**i) / math.factorial(i))
+        return num / den
+        """
+
+    def _get_state_0_probability(self):
+        if self.server_utilization >= 1:
+            return 0
+
+        state_zero_probability = (((self.num_servers * self.server_utilization)**self.num_servers) \
+                                / math.factorial(self.num_servers)) * (1 / (1 - self.server_utilization))
+        for i in range(self.num_servers):
+            state_zero_probability += ((self.num_servers * self.server_utilization)**i) / math.factorial(i)
+        return 1 / state_zero_probability
--- src/main.py
+++ src/main.py
@@ -1,21 +1,41 @@
-from MMC_system import MMC_system
+import statistics
+from MMcQueue import MMcQueue
 
 
-def print_results(queue_system):
-    print(f"Probabilita' di avere stato 0 = {queue_system.state_0_probability}")
-    print(f"Probabilita' di avere stato k = {queue_system.state_k_probability}")
-    print(f"Numero medio di pacchetti in coda: {queue_system.average_queue_length}")
-    print(f"Numero medio di pacchetti nel sistema: {queue_system.average_system_length}")
-    print(f"Tempo medio di attesa in coda: {queue_system.average_queue_waiting_time}")
-    print(f"Tempo medio di attesa nel sistema: {queue_system.average_system_waiting_time}")
+def print_calculated_parameters(queue):
+    average_service_length = statistics.mean(queue.system_lengths)
+    average_queue_length = statistics.mean(queue.queue_lengths)
+    average_system_length = average_service_length + average_queue_length
+    average_queue_waiting_time = statistics.mean(queue.queue_waiting_times)
+    average_system_waiting_time = statistics.mean(queue.system_waiting_times)
+    print("\n===============Parametri della simulazione===============")
+    print(f"Numero medio di pacchetti in servizio: {average_service_length}")
+    print(f"Numero medio di pacchetti in coda: {average_queue_length}")
+    print(f"Numero medio di pacchetti nel sistema: {average_system_length}")
+    print(f"Tempo medio di attesa in coda: {average_queue_waiting_time}")
+    print(f"Numero medio di attesa nel sistema: {average_system_waiting_time}")
+
+
+def print_parameters(queue):
+    print("\n=======Elementi caratteristici del sistema a coda=======")
+    print(f"Probabilitá di avere stato 0 = {queue.state_0_probability * 100}%")
+    print(f"Probabilitá di avere tutti i servitori occupati = {queue.queue_probability * 100}%")
+    print(f"Numero medio di pacchetti in servizio: {queue.average_service_length}")
+    print(f"Numero medio di pacchetti in coda: {queue.average_queue_length}")
+    print(f"Numero medio di pacchetti nel sistema: {queue.average_system_length}")
+    print(f"Tempo medio di attesa in coda: {queue.average_queue_waiting_time}")
+    print(f"Tempo medio di attesa nel sistema: {queue.average_system_waiting_time}")
 
 
 def main():
-    servers = int(input("Inserire il numero dei servitori: "))
+    num_servers = int(input("Inserire il numero dei servitori: "))
     arrival_rate = float(input("Inserire il tasso di nascita: "))
     service_rate = float(input("Inserire il tasso di morte: "))
-    queue_system = MMC_system(servers, arrival_rate, service_rate)
-    print_results(queue_system)
+    num_customers = int(input("Inserire il numero di clienti da simulare: "))
+    queue = MMcQueue(num_servers, arrival_rate, service_rate, num_customers)
+    queue.run()
+    print_parameters(queue)
+    print_calculated_parameters(queue)
     
 
 if __name__ == "__main__":
