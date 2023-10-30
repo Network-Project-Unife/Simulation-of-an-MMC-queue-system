975a31f52c0444bc614402d1993435ef901b7608
Alessio Celentano <alessiocelentano2003@gmail.com>
2023-10-30 13:44:29 +0100

    Add server description in graph and edit regex

--- src/main.py
+++ src/main.py
@@ -21,27 +21,65 @@ def draw_graph(queue):
     average_system_waiting_time = statistics.mean(queue.system_waiting_times)
     
     fig = go.Figure()
+
     fig.add_trace(go.Bar(
         x=times,
         y=customers_in_service,
-        name='Customers in service',
-        #marker_color='indianred'
+        name='Customers in service'
     ))
+
     fig.add_trace(go.Bar(
         x=times,
         y=customers_in_queue,
-        name='Customers in queue',
-        #marker_color='indianred'
-    ))    
+        name='Customers in queue'
+    ))
+    
+    legend_text = f"""\
+Numero dei servitori:<br>\
+{queue.num_servers}<br>\
+Tasso di nascita:<br>\
+{queue.arrival_rate}<br>\
+Tasso di morte:<br>\
+{queue.service_rate}<br>\
+# dei clienti da simulare:<br>\
+{queue.num_customers}<br><br>\
+PARAMETRI DELLA SIMULAZIONE<br>\
+# medio di pacchetti in servizio:<br>\
+{average_service_length}<br>\
+# medio di pacchetti in coda:<br>\
+{average_queue_length}<br>\
+# medio di pacchetti nel sistema:<br>\
+{average_system_length}<br>\
+t medio di attesa in coda:<br>\
+{average_queue_waiting_time}<br>\
+# medio di attesa nel sistema:<br>\
+{average_system_waiting_time}<br><br>\
+ELEMENTI CARATTERISTICI<br>\
+Probabilitá di avere stato 0:<br>\
+{queue.state_0_probability * 100}%<br>\
+Probabilitá coda:<br>\
+{queue.queue_probability * 100}%<br>\
+# medio di pacchetti in servizio:<br>\
+{queue.average_service_length}<br>\
+# medio di pacchetti in coda:<br>\
+{queue.average_queue_length}<br>\
+# medio di pacchetti nel sistema:<br>\
+{queue.average_system_length}<br>\
+t medio di attesa in coda:<br>\
+{queue.average_queue_waiting_time}<br>\
+t medio di attesa nel sistema:<br>\
+{queue.average_system_waiting_time}<br><br>\
+"""
+    
     fig.update_layout(
         title='Simulation of an M/M/C queue system',
-        xaxis=dict(title='Time [s]', type='category',),
-        yaxis=dict(title='Number of customers in the system'),
-        legend=dict(title="Customers in the system"),
-        barmode="stack"
+        xaxis=dict(title='Time [s]', type='category', visible=False),
+        yaxis=dict(title=f'Number of customers in the system'),
+        legend=dict(title=f"{legend_text}Customers in the system"),
+        barmode="stack",
+        bargap=0
     )
 
-    # Show the graph
     fig.show()
 
 
@@ -61,7 +99,7 @@ def get_input():
             if not inp:
                 messagebox.showerror("Errore", "Inserisci tutti i valori.")
                 return
-            if not re.match("[0-9]+(\.[0-9]+)?", inp):
+            if not re.match("^([1-9][0-9]*)?(0?\.[0-9]+)?$", inp):
                 messagebox.showerror("Errore", "Valori numerici non validi.")
                 return
         if (float(arrival_rate) / (int(num_servers) * float(service_rate))) >= 1:
@@ -72,7 +110,7 @@ def get_input():
         inp_list[1] = float(arrival_rate)
         inp_list[2] = float(service_rate)
         inp_list[3] = int(num_customers)
-        root.destroy()  # Close the dialog window
+        root.destroy()
 
     root = tk.Tk()
 
